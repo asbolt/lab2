180cb282d481b602ae0c7d6cf0e837828d64ad65
asbolt <asboltova@gmail.com>
2024-10-18 13:24:17 +0300

    Create precision_analysis.c

---
+++ precision_analysis.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+double precision_analysis (double *radioactivity, double *time, int N)
+       {
+           for (int numberElements = 10; numberElements <= N; numberElements++)
+           {
+                double decay_time = nonlinear_equation (radioactivity, time, numberElements, precision);
+                double decay_rate = linear_equation (radioactivity, time, numberElements);
+
+                if (2 * dev_exp (radioactivity, time, numberElements, decay_time) <=
+                   dev_linear(radioactivity, time, numberElements, decay_rate))
+                   return (radioactivity, time, numberElements - 1, precision);
+           }
+
+           return 1;
+       }
